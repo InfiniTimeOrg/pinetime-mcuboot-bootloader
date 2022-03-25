12d5a730dd5a96e5f9e1236f1d1f86f78be0ad02
Christoph Honal <christoph.honal@web.de>
2022-03-25 07:52:23 +0100

    Add P8 smartwatch configuration
    * Split into pinetime (default) and p8 target
    * Change LFCLK to LFRC to support devices without LF crystal
    * Support various SPI flash chips by ignoring JEDEC IDs
    * Use recent mynewt-core and mcu-tools

--- scripts/nrf52/build-boot.sh
+++ scripts/nrf52/build-boot.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 #  Build Mynewt bootloader on macOS and Linux
 
+TARGET=${1:-pinetime}
+echo "Building target $TARGET"
+
 set -e  #  Exit when any command fails.
 set -x  #  Echo all commands.
 
@@ -10,17 +13,17 @@ source paths.bash
 arm-none-eabi-gcc --version
 
 #  Build the bootloader.
-newt build nrf52_boot
+newt build $TARGET
 
 #  Show the size.
-newt size -v nrf52_boot
+newt size -v $TARGET
 
-arm-none-eabi-objcopy -I binary -O ihex bin/targets/nrf52_boot/app/@mcuboot/boot/mynewt/mynewt.elf.bin bin/targets/nrf52_boot/app/@mcuboot/boot/mynewt/mynewt.elf.hex
-scripts/hex2c.py bin/targets/nrf52_boot/app/@mcuboot/boot/mynewt/mynewt.elf.hex > reloader/src/boards/pinetime/bootloader.h
-make -C reloader BOARD=pinetime
+arm-none-eabi-objcopy -I binary -O ihex bin/targets/$TARGET/app/@mcuboot/boot/mynewt/mynewt.elf.bin bin/targets/$TARGET/app/@mcuboot/boot/mynewt/mynewt.elf.hex
+scripts/hex2c.py bin/targets/$TARGET/app/@mcuboot/boot/mynewt/mynewt.elf.hex > reloader/src/boards/$TARGET/bootloader.h
+make -C reloader build-$TARGET/reloader-mcuboot.zip all BOARD=$TARGET
 
 set +x
-echo "Bootloader firmware (elf) : bin/targets/nrf52_boot/app/@mcuboot/boot/mynewt/mynewt.elf"
-echo "Bootloader firmware (bin) : bin/targets/nrf52_boot/app/@mcuboot/boot/mynewt/mynewt.bin"
-echo "Bootloader firmware (hex) : bin/targets/nrf52_boot/app/@mcuboot/boot/mynewt/mynewt.hex"
-echo "Reloader (DFU) : reloader/build-pinetime/reloader-mcuboot.zip"
+echo "Bootloader firmware (elf) : bin/targets/$TARGET/app/@mcuboot/boot/mynewt/mynewt.elf"
+echo "Bootloader firmware (bin) : bin/targets/$TARGET/app/@mcuboot/boot/mynewt/mynewt.bin"
+echo "Bootloader firmware (hex) : bin/targets/$TARGET/app/@mcuboot/boot/mynewt/mynewt.hex"
+echo "Reloader (DFU) : reloader/build-$TARGET/reloader-mcuboot.zip"
